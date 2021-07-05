3557fb15cd24d83f4ee3a5f3cbe35c05e4260f43
ikizey <ikizey@icloud.com>
2021-07-05 16:55:12 +0300

    move actions to own class

--- tic-tac-toe-stars/BoardCell.swift
+++ tic-tac-toe-stars/BoardCell.swift
@@ -6,49 +6,84 @@
 //
 
 import SpriteKit
-import AVFoundation
 
 class BoardCell: SKShapeNode {
 
     var index: Int!
     
+    let action = Action.do
+    
     private var image: SKSpriteNode!
     
-    private let spinAction = SKAction.rotate(byAngle: .pi * CGFloat.random(in: 2...4),
-                                             duration: 0.5)
-    
-    private let scaleInAction = SKAction.scale(by: 1.2, duration: 0.25)
-    private let scaleOutAction = SKAction.scale(by: 0, duration: 1)
-    
-    private let removeAction = SKAction.customAction(withDuration: 1, actionBlock: { node, duration in
-        node.removeFromParent()
-    })
-    
     func spinAndScale() {
-        let spinAndScaleAc = SKAction.group([spinAction, scaleInAction])
-        image.run(SKAction.sequence([spinAndScaleAc, scaleInAction.reversed()]))
+        image.run(action.spinAndScale)
     }
     
     func spin() {
-        image.run(spinAction)
+        image.run(Action.spin)
     }
     
     func mark(with texture: SKTexture) {
         image = SKSpriteNode(texture: texture)
-        self.addChild(image)
+        addChild(image)
         playSound()
         spin()
     }
     
     func unmark() {
         guard let image = image else { return }
-        
-        let actions = [scaleOutAction, removeAction]
-        image.run(SKAction.sequence(actions))
+
+        image.run(action.remove)
     }
     
     func playSound() {
-        let soundName = "Move.wav"
-        self.run(.playSoundFileNamed(soundName, waitForCompletion: false))
+        run(action.playTapSound)
     }
+}
+
+// MARK: - Actions
+
+extension BoardCell {
+    
+    struct Action {
+        
+        static let `do` = Action()
+        
+        
+        let scaleIn: SKAction
+        
+        let scaleOut: SKAction
+        
+        let remove: SKAction
+        
+        let spinAndScale: SKAction
+        
+        static var spin: SKAction {
+            //let direction = CGFloat([-1, 1].randomElement()!) // not fun
+            let rotations = CGFloat.random(in: 2...4)
+            let action = SKAction.rotate(byAngle: /*direction * */.pi * rotations, duration: 0.5)
+            return action
+        }
+        
+        let playTapSound: SKAction
+        
+        private init() {
+            scaleIn = SKAction.scale(by: 1.2, duration: 0.25)
+            
+            scaleOut = SKAction.scale(by: 0, duration: 1)
+            
+            let rm = SKAction.customAction(withDuration: 1, actionBlock: { node, duration in
+                node.removeFromParent()
+            })
+            remove = SKAction.sequence([scaleOut, rm])
+            
+            let spinAndScaleGroup = SKAction.group([Action.spin, scaleIn])
+            spinAndScale = SKAction.sequence([spinAndScaleGroup,
+                                              spinAndScaleGroup.reversed()])
+            
+            let soundName = "Move.wav"
+            playTapSound = .playSoundFileNamed(soundName, waitForCompletion: false)
+        }
+    }
+    
 }
